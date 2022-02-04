b4d5ca2d5f513a6a917dd3eb3b5057be9ac57bde
Mieszko Matuszczak <mieszko@amcreative.pl>
2022-02-04 11:45:58 +0100

    update

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,20 +1,18 @@
 <template>
-    <ul
-        class="flex flex-col md:flex-row space-y-4 md:space-y-0 items-center justify-end font-liber"
-    >
+    <ul class="flex flex-col md:flex-row items-center justify-end font-liber">
         <li>
-            <a href="#" class="p-6 hover:underline">Spis treści</a>
+            <a href="#" class="p-3 2xl:p-4 hover:underline">Spis treści</a>
         </li>
         <li>
-            <a href="#" class="p-6 hover:underline">O nas</a>
+            <a href="#" class="p-3 2xl:p-4 hover:underline">O nas</a>
         </li>
         <li>
-            <a href="#" class="p-6 hover:underline">Kontakt</a>
+            <a href="#" class="p-3 2xl:p-4 hover:underline">Kontakt</a>
         </li>
         <li>
             <router-link
                 to="#"
-                class="font-liber inline-flex items-center justify-center rounded-full border border-transparent px-8 py-1 text-base font-semibold leading-7 transition duration-150 ease-in-out focus:outline-none text-center bg-black text-white ml-6"
+                class="font-liber inline-flex items-center justify-center rounded-full border border-transparent px-5 py-1 text-base font-semibold leading-7 transition duration-150 ease-in-out focus:outline-none text-center bg-black text-white ml-4"
             >
                 Zaloguj się</router-link
             >
--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -2,14 +2,14 @@
     <header
         class="flex flex-col sm:flex-row justify-center items-center sm:justify-between p-4 sm:p-8"
     >
-        <div class="w-1/3 hidden 2xl:block"></div>
-        <div class="w-full sm:w-1/2 md:w-1/3 2xl:w-1/3 text-center font-liber">
+        <div class="w-1/3 hidden xl:block"></div>
+        <div class="w-full sm:w-1/2 md:w-1/3 xl:w-1/3 text-center font-liber">
             <h2 class="-mt-1">Interaktywna książka</h2>
-            <h1 class="font-medium text-lg md:text-xl lg:text-2xl font-liber">
+            <h1 class="font-medium text-lg md:text-xl lg:text-xl font-liber">
                 O mowie nienawiści i hejcie
             </h1>
         </div>
-        <div class="w-full sm:w-1/2 md:w-2/3 2xl:w-1/3">
+        <div class="w-full sm:w-1/2 md:w-2/3 xl:w-1/3">
             <Navigation class="hidden lg:flex" />
             <NavigationMobile class="lg:hidden" />
         </div>

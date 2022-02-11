6585ed9a6d8103876e7fedd57c4ada8af84fffe6
Mieszko Matuszczak <mieszko@amcreative.pl>
2022-02-11 17:51:55 +0100

    updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
                 class="relative max-w-[500px] w-[500px] sm:max-w-none sm:w-[800px] md:w-[1000px] lg:w-[1300px] xl:w-[1400px] mx-auto z-10"
             />
             <div
-                class="absolute bottom-[33px] sm:bottom-[55px] md:bottom-[70px] lg:bottom-[91px] xl:bottom-[93px] left-[-50px] w-[150%] origin-bottom-left h-full bg-[#E45725] z-0 transform rotate-[357deg]"
+                class="absolute bottom-[33px] sm:bottom-[55px] md:bottom-[70px] lg:bottom-[91px] xl:bottom-[99px] 2xl:bottom-[80px] left-[-50px] w-[150%] origin-bottom-left h-full bg-[#E45725] z-0 transform rotate-[357deg]"
             ></div>
         </div>
         <div class="flex justify-center items-center">

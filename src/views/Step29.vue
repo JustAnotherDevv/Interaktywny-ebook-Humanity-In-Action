c557b458e23442c69ce0da3b62c59e525270cabb
Mieszko Matuszczak <mieszko@amcreative.pl>
2022-02-11 16:49:11 +0100

    update

---
+++ src/views/Step29.vue
@@ -0,0 +1,53 @@
+<template>
+    <Step hide-skip-button>
+        <div class="space-y-8">
+            <img
+                src="/assets/29/wnioski.svg"
+                width="417"
+                height="111"
+                alt=""
+                class="mx-auto"
+            />
+            <p class="max-w-2xl md:text-center">
+                Mowa nienawiści wbrew pozorom nie jest „wynalazkiem” naszych
+                czasów. W historii ludzkości była niestety wielokrotnie używana.
+                Zazwyczaj po to, by ułatwić grupie dominującej skuteczne
+                zdobycie i utrzymanie władzy pod wodzą lidera i jego
+                popleczników realizujących bez skrupułów swój plan. Historia
+                uczy, że nakręcanie spirali nienawiści i pogardy pomiędzy
+                różnymi grupami jest niebezpieczne, często prowadząc do
+                nieodwracalnych skutków.
+            </p>
+            <p class="max-w-2xl md:text-center">
+                W niektórych przypadkach doprowadziło do ludobójstwa, jak w
+                przypadku Holokaustu, ludobójstwa w Namibii, Kambodży, Rwandzie,
+                czy b. Jugosławii (w Srebrenicy). Amerykański profesor prawa
+                Gregory Stanton prowadził badania nad ludobójstwami w różnych
+                krajach i na podstawie tych obserwacji stworzył model, zwany
+                teorią 10 etapów prowadzących do ludobójstwa, za pomocą której
+                można lepiej zrozumieć poszczególne fazy przygotowań, by lepiej
+                przewidywać niebezpieczeństwo wystąpienia ludobójstwa, a także
+                skuteczniej mu zapobiegać.
+            </p>
+            <img
+                src="/assets/29/cwiczenie.svg"
+                width="398"
+                height="67"
+                alt=""
+                class="mx-auto"
+            />
+
+            <p class="font-amatic text-3xl max-w-2xl md:text-center">
+                Pewnie sobie pomyślisz: po co mi ta teoria, skoro żyjemy w XXI
+                w. i Polska jest krajemy na ogół przestrzegającym prawa
+                człowieka?! Przyjrzyj się uważnie pierwszym 4 etapom i zastanów
+                się, czy dostrzegasz jakieś podobieństwa do współczesności i
+                bieżących wydarzeń
+            </p>
+        </div>
+    </Step>
+</template>
+
+<script setup>
+import Step from "@/components/Step.vue";
+</script>
---
+++ src/views/Step30.vue
@@ -0,0 +1,57 @@
+<template>
+    <Step hide-skip-button>
+        <div class="relative">
+            <img
+                src="/assets/30/top.svg"
+                class="fixed top-0 left-0 hidden lg:block"
+                alt=""
+                width="120"
+                height="370"
+            />
+            <img
+                src="/assets/30/bottom.svg"
+                class="fixed bottom-0 right-0 hidden lg:block"
+                alt=""
+                width="120"
+                height="370"
+            />
+            <img
+                src="/assets/30/etapy.svg"
+                alt=""
+                width="183"
+                height="96"
+                class="mx-auto mb-10"
+            />
+            <img
+                src="/assets/30/teoria.svg"
+                width="260"
+                height="192"
+                alt=""
+                class="mx-auto mb-10"
+            />
+            <img
+                src="/assets/30/separator.svg"
+                width="504"
+                height="15"
+                alt=""
+                class="mx-auto mb-10"
+            />
+            <p class="text-lg max-w-3xl md:text-center">
+                Procesy prowadzące do ludobójstwa rozwijają się w
+                przewidywalnych fazach, które jednak nie zawsze występują jedna
+                po drugiej. Czasem wręcz nakładają się na siebie i występują
+                jednocześnie. Zwykle etapy następują jeden po drugim, chociaż w
+                niektórych wypadkach konkretne etapy mogą trwać przez cały
+                proces. Czytając opisy poszczególnych etapów, zwróć szczególną
+                uwagę na mechanizmy, które niestety powtarzają się niezależnie
+                od czasu i miejsca występowania. Pamiętaj, to błędne koło można
+                zatrzymać na każdym etapie poprzedzającym śmierć pierwszych
+                ofiar, potem jest już za późno!
+            </p>
+        </div>
+    </Step>
+</template>
+
+<script setup>
+import Step from "@/components/Step.vue";
+</script>

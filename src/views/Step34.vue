fdcfbbfc949ffcffb9b1262c55c3cc19741f69ea
Mieszko Matuszczak <mieszko@amcreative.pl>
2022-02-14 15:26:13 +0100

    update

---
+++ src/views/Step34.vue
@@ -0,0 +1,60 @@
+<template>
+    <Step hide-skip-button>
+        <img
+            src="/assets/34/czy-wiesz-ze.svg"
+            alt=""
+            width="416"
+            height="91"
+            class="mb-10"
+        />
+        <p class="md:text-center max-w-2xl">
+            Najbardziej kompleksowe badania, które opisują skalę mowy nienawiści
+            w Polsce zostały przeprowadzone przez Centrum Badań nad
+            Uprzedzeniami Uniwersytetu Warszawskiego w 2014 i 2016 r. Ich celem
+            było m.in. sprawdzenie, w jakich miejscach Polacy i Polki spotykają
+            się z nienawistnymi wypowiedziami oraz czy skala mowy nienawiści
+            wzrosła, czy zmalała w stosunku do podobnych badań z 2014 r.
+        </p>
+
+        <div class="bg-red-900 p-16 w-full text-center my-10">Wykresy</div>
+
+        <p class="md:text-center max-w-2xl">
+            Jak możesz zobaczyć, w ciągu 2 lat poziom mowy nienawiści wobec
+            wszystkich badanych grup znacznie wzrósł, zarówno wśród dorosłych
+            jak i młodzieży. W przypadku Ukraińców oraz muzułmanów jest to
+            wzrost o aż 24,5%! Dlaczego tak się stało?
+        </p>
+
+        <div class="bg-red-900 p-16 w-full text-center my-10">Wykresy</div>
+
+        <img
+            src="/assets/34/arrow.svg"
+            alt=""
+            width="264"
+            height="222"
+            class="mb-10"
+        />
+
+        <p class="md:text-center max-w-2xl">
+            W 2015 r. miały miejsce wybory parlamentarne i prezydenckie, a język
+            kampanii wyborczej przepełniony był nienawistnymi komentarzami w
+            stosunku do „Innych”, które wzmacniały negatywne, ksenofobiczne
+            postawy. W większości, ofiarami tej narracji byli uchodźcy, będący w
+            większości muzułmanami, którzy w tamtym czasie licznie przybywali do
+            Europy z Afryki Północnej.
+        </p>
+
+        <div
+            class="bg-black text-white p-8 my-10 text-lg max-w-2xl text-center"
+        >
+            A teraz zobacz, gdzie regularnie spotykasz mowę nienawiści, być może
+            tego nie zauważając:
+        </div>
+
+        <img src="/assets/34/stats.svg" alt="" />
+    </Step>
+</template>
+
+<script setup>
+import Step from "@/components/Step.vue";
+</script>

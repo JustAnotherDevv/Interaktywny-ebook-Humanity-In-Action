224f0681769057fdf4e370716498ee7f6d5fba42
Mieszko Matuszczak <mieszko@amcreative.pl>
2022-02-14 15:18:52 +0100

    update

---
+++ src/views/Step33.vue
@@ -0,0 +1,33 @@
+<template>
+    <Step hide-skip-button>
+        <img
+            src="/assets/33/do-refleksji.svg"
+            alt=""
+            width="339"
+            height="98"
+            class="mb-10"
+        />
+        <div class="bg-black text-white p-6 md:px-20 text-xl mb-10 text-center">
+            A jak się ma ta pozornie odległa historia do czasów współczesnych?
+        </div>
+
+        <div
+            class="sm:px-20 sm:py-10 sm:bg-[url(/assets/33/phone.svg)] bg-cover bg-no-repeat sm:w-96 h-[500px]"
+        >
+            <p>
+                Ponad 50 lat później wciąż możemy zaobserwować „kampanie
+                nienawiści” skierowane przeciwko konkretnym grupom
+                mniejszościowym, które dodatkowo wzmacniają i tak już obecne
+                stereotypy i uprzedzenia ich dotyczące. Wobec jakich grup
+                stosowano w ostatnich latach dehumanizujący język, które z nich
+                przedstawiane są publicznie negatywnie? W jaki sposób
+                współczesne kampanie są podobne do tej z 1968 r., a czym się
+                różnią?
+            </p>
+        </div>
+    </Step>
+</template>
+
+<script setup>
+import Step from "@/components/Step.vue";
+</script>

d82cbadeee7a951b47353e9eebb99530f375c124
Mieszko Matuszczak <mieszko@amcreative.pl>
2022-02-16 16:26:05 +0100

    update

---
+++ src/views/Step62.vue
@@ -0,0 +1,80 @@
+<template>
+    <Step class="font-montserrat" hide-skip-button>
+        <div class="flex flex-wrap md:flex-nowrap">
+            <img
+                src="/assets/62/w-jaki-sposob.svg"
+                alt=""
+                width="317"
+                height="176"
+                class="mx-auto"
+            />
+            <img
+                src="/assets/62/person.svg"
+                alt=""
+                width="237"
+                height="250"
+                class="mx-auto"
+            />
+        </div>
+        <p class="font-amatic text-4xl mt-6">Jeśli sprawca jest nam znany...</p>
+
+        <div
+            class="p-6 pt-10 md:p-10 md:pt-16 border-2 border-black relative mt-20"
+        >
+            <div
+                class="sm:absolute top-0 transform -translate-y-1/2 md:left-[150px] left-[10px] bg-black text-white p-2 font-itim text-xl md:text-3xl inline-block"
+            >
+                I Sposób - droga cywilna
+            </div>
+            <p>
+                Na podstawie
+                <strong>art. 23 w zw. z art. 24 Kodeksu cywilnego</strong>
+                każdemu człowiekowi i osobie prawnej przysługuje ochrona dóbr
+                osobistych, w tym m.in. godności i dobrego imienia. Proces o
+                naruszenie dóbr osobistych toczy się w sądzie cywilnym. W takiej
+                sytuacji należy wstąpić na drogę cywilnoprawną, czyli wnieść
+                pozew do sądu cywilnego. W przypadku naruszenia dóbr osobistych
+                danej osoby fizycznej lub prawnej, osoba, której dobro osobiste
+                jest zagrożone cudzym działaniem może żądać, aby dana osoba
+                zaniechała takiego działania. Żądanie zaniechania działania nie
+                dotyczy działań już dokonanych, ale sytuacji, gdy występuje
+                zagrożenie bezprawnego działania naruszającego dobro osobiste.
+                Natomiast, jeżeli czyjeś dobro zostało już naruszone, czyli w
+                sytuacji, gdy zamieszczono w sieci treść, która jest mową
+                nienawiści, można wówczas nakazać, aby osoba, która dokonała
+                takiego naruszenia, podjęła czynności mające na celu usunięcie
+                skutków jej działania, np. poprzez publiczne przeproszenie,
+                wydanie sprostowania lub usunięcie treści, jeżeli nadal jest
+                dostępna na stronie internetowej. Osoba pokrzywdzona może żądać
+                też zadośćuczynienia pieniężnego lub zapłaty odpowiedniej sumy
+                pieniężnej (dla pokrzywdzonego/ej lub na wskazaną przez niego
+                organizację charytatywną/społeczną). W przypadku, gdy mowa
+                nienawiści spowodowała, że dana osoba poniosła szkodę majątkową,
+                może ona żądać odszkodowania na zasadach ogólnych.
+            </p>
+        </div>
+
+        <div class="flex flex-wrap md:flex-nowrap my-10 md:mx-20">
+            <img
+                src="/assets/62/wazne.svg"
+                height="120"
+                width="280"
+                alt=""
+                class="md:mr-10 mb-10 md:mb-0 mx-auto"
+            />
+            <p>
+                Ochrona ta przysługuje wyłącznie pokrzywdzonemu/ej i tylko w
+                przypadku, gdy jest to działanie bezprawne! Bezprawność
+                działania występuje wtedy, gdy jest ono sprzeczne z normami
+                prawnymi lub zasadami współżycia społecznego.
+            </p>
+        </div>
+        <div>
+            <img src="/assets/62/czy-wiesz-ze.svg" alt="" height="342" />
+        </div>
+    </Step>
+</template>
+
+<script setup>
+import Step from "@/components/Step.vue";
+</script>

0780ebcd8e56004df670f3b59d6ef324787c9ce0
Mieszko Matuszczak <mieszko@amcreative.pl>
2022-02-17 13:24:14 +0100

    update

---
+++ src/views/Step67.vue
@@ -0,0 +1,120 @@
+<template>
+    <Step class="font-montserrat" hide-skip-button>
+        <img src="/assets/67/co-mozesz.svg" alt="" />
+        <div class="max-w-md space-y-16 sm:space-y-8 mt-10">
+            <!--  -->
+            <div class="relative">
+                <img
+                    src="/assets/67/arrow-1.svg"
+                    alt=""
+                    class="sm:absolute mx-auto mb-4 sm:mb-0 top-[-40px] left-[10px]"
+                />
+                <p class="sm:pl-24">
+                    Zacząć od siebie i świecić samemu/samej przykładem na tyle,
+                    na ile w danym momencie dasz radę.
+                </p>
+            </div>
+            <!--  -->
+            <!--  -->
+            <div class="relative">
+                <img
+                    src="/assets/67/arrow-2.svg"
+                    alt=""
+                    class="sm:absolute mx-auto mb-4 sm:mb-0 top-0 left-[-30px]"
+                />
+                <p class="sm:pl-24">
+                    Włączać się na ile się da w działania przeciw mowie
+                    nienawiści i hejtowi, czerpać od innych inspiracje.
+                </p>
+            </div>
+            <!--  -->
+            <!--  -->
+            <div class="relative">
+                <img
+                    src="/assets/67/arrow-3.svg"
+                    alt=""
+                    class="sm:absolute mx-auto mb-4 sm:mb-0 top-0 left-[-30px]"
+                />
+                <p class="sm:pl-24">
+                    Pokazywać innym, że mowa nienawiści i hejt mają realny
+                    negatywny wpływ na innych ludzi.
+                </p>
+            </div>
+            <!--  -->
+            <!--  -->
+            <div class="relative">
+                <img
+                    src="/assets/67/arrow-4.svg"
+                    alt=""
+                    class="sm:absolute mx-auto mb-4 sm:mb-0 top-[20px] left-0"
+                />
+                <p class="sm:pl-24">
+                    Uświadamiać sprawcom/czyniom mowy nienawiści i hejtu, że ich
+                    poczucie anonimowości w sieci jest jedynie pozorne i że
+                    muszą się liczyć z konsekwencjami prawnymi.
+                </p>
+            </div>
+            <!--  -->
+            <!--  -->
+            <div class="relative">
+                <img
+                    src="/assets/67/arrow-5.svg"
+                    alt=""
+                    class="sm:absolute mx-auto mb-4 sm:mb-0 top-[20px] left-[10px]"
+                />
+                <p class="sm:pl-24">
+                    Przyczyniać się do zmniejszania ilości mowy nienawiści i
+                    hejtu: dokumentować ich przykłady i zgłaszać administratorom
+                    portali, stosownym władzom czy organizacjom pozarządowym.
+                </p>
+            </div>
+            <!--  -->
+            <!--  -->
+            <div class="relative">
+                <img
+                    src="/assets/67/arrow-6.svg"
+                    alt=""
+                    class="sm:absolute mx-auto mb-4 sm:mb-0 top-[20px] left-[10px]"
+                />
+                <p class="sm:pl-24">
+                    Reagować na mowę nienawiści i hejt i angażować innych do
+                    reagowania razem z Tobą, by większą grupą reagować na daną
+                    sytuację. Wtedy sprzeciw jest bardziej zdecydowany, a tym
+                    samym bardziej „słyszalny”.
+                </p>
+            </div>
+            <!--  -->
+            <!--  -->
+            <div class="relative">
+                <img
+                    src="/assets/67/arrow-7.svg"
+                    alt=""
+                    class="sm:absolute mx-auto mb-4 sm:mb-0 top-0 left-[40px]"
+                />
+                <p class="sm:pl-24">
+                    Solidarnie wspierać osoby atakowane: uświadamiać, że nie są
+                    same, bo mają sojuszników/czki gotowych/we do pomocy.
+                </p>
+            </div>
+            <!--  -->
+            <!--  -->
+            <div class="relative">
+                <img
+                    src="/assets/67/arrow-8.svg"
+                    alt=""
+                    class="sm:absolute mx-auto mb-4 sm:mb-0 top-0 left-[10px]"
+                />
+                <p class="sm:pl-24">
+                    Jeśli to Ty jesteś atakowany/a mową nienawiści i hejtem i
+                    nie masz już siły się z tym zmagać – porozmawiaj z kimś
+                    zaufanym. Zasygnalizuj, że potrzebujesz wsparcia!
+                </p>
+            </div>
+            <!--  -->
+        </div>
+    </Step>
+</template>
+
+<script setup>
+import Step from "@/components/Step.vue";
+</script>

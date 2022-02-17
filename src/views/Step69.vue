9c9df13731dd70d285ccb7bffa6398d4de998e7f
Mieszko Matuszczak <mieszko@amcreative.pl>
2022-02-17 14:39:25 +0100

    changes

---
+++ src/views/Step69.vue
@@ -0,0 +1,47 @@
+<template>
+    <Step class="font-montserrat" hide-skip-button>
+        <div class="flex flex-wrap md:flex-nowrap">
+            <img src="/assets/69/ola.svg" alt="" class="mx-auto" />
+            <div class="md:pl-10 mt-10">
+                <p class="font-amatic text-4xl md:max-w-[280px] mb-10">
+                    Jak rozumiesz/definiujesz aktywizm i dlaczego jest Twoim
+                    zdaniem ważny?
+                </p>
+                <p>
+                    "Aktywizm to działanie wspierające konkretny cel, idee lub
+                    sprawę. Można zaangażować się w sprawy, które bezpośrednio
+                    nas dotyczą lub też wspierać te, które oddziałują na nasze
+                    otoczenie lub naszych bliskich. Mogą być to zarówno tematy
+                    polityczne, społeczne. Takie działania podejmowane są
+                    zazwyczaj z potrzeby wyrażenia swojego głosu, manifestowania
+                    opinii. Nie jest to związane z żadnym zyskiem finansowym lub
+                    pracą. Robimy to, bo chcemy to robić. Chcemy zmienić świat i
+                    wyrazić swoje wsparcie dla tych, którzy tego potrzebują”.
+                </p>
+            </div>
+        </div>
+        <div class="mt-10 w-full relative md:mb-20">
+            <p class="font-amatic text-4xl md:max-w-[280px] mb-10">
+                Jak być dobrym sojusznikiem/czką grup dyskryminowanych?
+            </p>
+            <p>
+                Przede wszystkim należy być odważnym. Nie bać się stanąć w
+                obronie tych, którzy czują się wykluczeni społecznie lub nie
+                mają równych praw. Ważne, by zapoznać się z tematem, o co te
+                grupy walczą, czego potrzebują, co stoi na ich przeszkodzie.
+                Należy pamiętać aby ich głos był podbijany naszym, a nie
+                zagłuszany. Zadawać pytania, uczyć się, a później przekazywać tą
+                wiedzę innym. Należy być otwartym na inność i różnorodność”.
+            </p>
+            <img
+                src="/assets/69/quote.svg"
+                class="hidden md:block absolute right-0 bottom-[-50px]"
+                alt=""
+            />
+        </div>
+    </Step>
+</template>
+
+<script setup>
+import Step from "@/components/Step.vue";
+</script>
---
+++ src/views/Step70.vue
@@ -0,0 +1,94 @@
+<template>
+    <Step class="font-montserrat" hide-skip-button>
+        <div class="flex flex-wrap md:flex-no-wrap md:space-x-4">
+            <img
+                src="/assets/70/jak-mozna.svg"
+                width="335"
+                height="126"
+                alt=""
+                class="mx-auto mb-6 md:mb-0"
+            />
+            <img
+                src="/assets/70/ciekawe.svg"
+                width="335"
+                height="126"
+                alt=""
+                class="mx-auto"
+            />
+        </div>
+
+        <div class="max-w-3xl mt-10">
+            <div
+                class="md:bg-[url(/assets/70/mowa-milosci-frame.svg)] bg-contain bg-no-repeat md:h-[200px] md:px-16 md:py-12 lg:py-16 p-10 md:text-center border-2 border-black md:border-0"
+            >
+                <img
+                    src="/assets/70/mowa-milosci.svg"
+                    alt=""
+                    class="mb-6 md:hidden"
+                />
+                <p>
+                    to akcja Fundacji Klamra polegająca na tworzeniu murali,
+                    które przeciwstawiają się mowie nienawiści wobec grup na nią
+                    narażonych.
+                </p>
+            </div>
+        </div>
+
+        <div class="max-w-3xl mt-10 w-full">
+            <div class="flex flex-wrap">
+                <div class="w-full md:w-3/5">
+                    <div
+                        class="md:bg-[url(/assets/70/zamaluj-frame.svg)] bg-contain bg-no-repeat md:h-[250px] p-10 md:pt-24 border-2 border-black md:border-0"
+                    >
+                        <img
+                            src="/assets/70/zamaluj.svg"
+                            alt=""
+                            class="mb-6 md:hidden"
+                        />
+                        <p class="md:w-[350px]">
+                            inicjatywy w różnych miastach polegające na
+                            namierzaniu nienawistnych napisów na budynkach,
+                            dokumentowanie ich i doprowadzanie do legalnego ich
+                            zamalowywania.
+                        </p>
+                    </div>
+                    <div
+                        class="md:bg-[url(/assets/70/pisanie-frame.svg)] bg-contain bg-no-repeat md:h-[250px] mt-10 p-10 md:pt-24 border-2 border-black md:border-0"
+                    >
+                        <img
+                            src="/assets/70/pisanie.svg"
+                            alt=""
+                            class="mb-6 md:hidden"
+                        />
+                        <p class="md:w-[350px]">
+                            wpadających w ucho piosenek np. hip-hopowych, które
+                            wspierają działania anty-hejtowe i mogą np. być
+                            ustawione jako dzwonek w telefonie.
+                        </p>
+                    </div>
+                </div>
+                <div class="w-full md:w-2/5">
+                    <div
+                        class="md:bg-[url(/assets/70/tworzenie-frame.svg)] bg-contain bg-no-repeat md:h-[450px] mt-10 md:mt-0 p-10 md:pt-32 sm:pt-24 border-2 border-black md:border-0"
+                    >
+                        <img
+                            src="/assets/70/zamaluj.svg"
+                            alt=""
+                            class="mb-6 md:hidden"
+                        />
+                        <p class="md:w-[240px] transform -rotate-[3deg]">
+                            których chętne osoby mogą łatwo użyć, gdy chcą
+                            przeciwstawić się np. jakiemuś wpisowi czy
+                            komentarzowi w mediach społecznościowych, który
+                            promuje lub zawiera elementy mowy nienawiści.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Step>
+</template>
+
+<script setup>
+import Step from "@/components/Step.vue";
+</script>
